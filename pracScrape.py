37a25c035a92f1f2658313a21833b84fa1797df9
Bridget Asser <bridgetasser@gmail.com>
2020-03-09 19:10:56 -0400

    refactor into function for scale

--- pracScrape.py
+++ pracScrape.py
@@ -1,18 +1,33 @@
-import bs4
+#commented code can be ran in terminal 
 
-import requests
+# import bs4
 
-res = requests.get('https://www.amazon.com/Bhagavad-Gita-2nd-Eknath-Easwaran/dp/1586380192/')
+# import requests
 
-res.raise_for_status()
+# res = requests.get('https://www.amazon.com/Bhagavad-Gita-2nd-Eknath-Easwaran/dp/1586380192/')
 
-bs4.BeautifulSoup(res.text)
+# res.raise_for_status()
 
-soup = bs4.BeautifulSoup(res.text)
+# bs4.BeautifulSoup(res.text)
 
-soup.select('#mediaNoAccordion > div.a-row > div.a-column.a-span4.a-text-right.a-span-last > span.a-size-medium.a-color-price.header-price')
+# soup = bs4.BeautifulSoup(res.text)
 
-elems[0].text.strip()
+# soup.select('#mediaNoAccordion > div.a-row > div.a-column.a-span4.a-text-right.a-span-last > span.a-size-medium.a-color-price.header-price')
+
+# elems[0].text.strip()
 
 
+import bs4, requests 
 
+def getAmazonPrice(productUrl):
+    res = requests.get(productUrl)
+    res.raise_for_status()
+
+    soup = bs4.BeautifulSoup(res.text, 'html.parser')
+    elems = soup.select('#mediaNoAccordion > div.a-row > div.a-column.a-span4.a-text-right.a-span-last > span.a-size-medium.a-color-price.header-price')
+    #should add a try/catch block or debugger break point for css updates 
+    elems[0].text.strip()
+
+
+price = getAmazonPrice('https://www.amazon.com/Bhagavad-Gita-2nd-Eknath-Easwaran/dp/1586380192/')
+print(f'The cost is {price}')
